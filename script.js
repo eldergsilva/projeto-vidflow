476eb0ef79079f83705985c8df67310038721840
unknown <eldergsilva@gmail.com>
2024-04-09 12:45:20 -0300

    optimizado a função filtrarPesquisa

--- script.js
+++ script.js
@@ -25,17 +25,22 @@ async function buscarEmostrarVideos(){
  
       
 }
+
 buscarEmostrarVideos();
 
- const barraDePesquisa = document.querySelector(".pesquisar__input");
+const barraDePesquisa = document.querySelector(".pesquisar__input");
 
- barraDePesquisa.addEventListener("input,filtrarPesquisa")
 
- function filtrarPesquisa (){
-     const videos  =document.querySelectorAll(".videos__item")
-     if (barraDePesquisa.value !="" ) {
-               
-     }else{
-        
-     }
- }
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+  
+    videos.forEach((video) => {
+      const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+  
+      video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+  }
